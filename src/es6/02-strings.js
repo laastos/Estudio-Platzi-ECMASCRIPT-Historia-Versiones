64de2b38e3676a654d60e7f4eee32e3f0bf642c5
Libardo Arroyave <laastos@gmail.com>
2023-02-16 21:37:04 -0500

    WIP

---
+++ src/es6/02-strings.js
@@ -0,0 +1,10 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+let lorem = `Esto es un string \nEsto es otra lìnea`;
+console.log(lorem);
